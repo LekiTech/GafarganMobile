759523ff77c107d052996723431ae63fa432fff1
ktadzjibov <ktadzjibov@gmail.com>
2022-11-18 01:38:49 +0100

    initial commit

---
+++ .prettierrc.js
@@ -0,0 +1,17 @@
+// module.exports = {
+//   arrowParens: 'avoid',
+//   bracketSameLine: true,
+//   bracketSpacing: false,
+//   singleQuote: true,
+//   trailingComma: 'all',
+// };
+module.exports = {
+  "trailingComma": "all",
+  "tabWidth": 2,
+  "semi": true,
+  "singleQuote": true,
+  "bracketSpacing": true,
+  "printWidth": 100,
+  "useTabs": false,
+  "endOfLine": "lf"
+}
